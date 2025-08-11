4d6ae3c5d0fe8a5ca3633d881cc65f8c37ed5032
DESKTOP-PIHD0PU\maml_ <marcomldeveloper@gmail.com>
2025-08-10 20:22:40 -0600

    chore: inicializa proyecto React con Vite y componentes base (NavBar, CartWidget, ItemListContainer)

---
+++ CreaTuLanding1mena/src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import CartWidget from './CartWidget.jsx'
+
+function NavBar() {
+  return (
+    <header className="navbar">
+      <div className="navbar__brand">
+        <a href="/" className="navbar__logo">MiTienda</a>
+      </div>
+      <nav className="navbar__nav">
+        <a href="#home" className="navbar__link">Inicio</a>
+        <a href="#catalogo" className="navbar__link">Catálogo</a>
+        <a href="#ofertas" className="navbar__link">Ofertas</a>
+        <a href="#contacto" className="navbar__link">Contacto</a>
+      </nav>
+      <div className="navbar__actions">
+        <CartWidget count={0} />
+      </div>
+    </header>
+  )
+}
+
+export default NavBar
+
+
